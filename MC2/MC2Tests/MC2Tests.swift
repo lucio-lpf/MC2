3ec94cb86666505fe5fd81a3bb777bd079f5af72
lucio-lpf <lucio.lpf@gmail.com>
2015-06-09 18:04:48 -0300

    Starting Project

---
+++ MC2/MC2Tests/MC2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  MC2Tests.swift
+//  MC2Tests
+//
+//  Created by Lúcio Pereira Franco on 09/06/15.
+//  Copyright (c) 2015 Grupo MC2. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class MC2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
