a2a4e21518e3e7953bb942ffdb04a7875cf60f80
lucio-lpf <lucio.lpf@gmail.com>
2015-06-24 14:36:17 -0300

    adicionando funções para salvar as historias e pedaços delas

--- src/Our Story/Our Story/StoryPiece.swift
+++ src/Our Story/Our Story/StoryPiece.swift
@@ -13,4 +13,20 @@ class StoryPiece: NSObject {
     var text: String?
     var createdBy: User?
     var createdAt: NSDate?
+    
+    
+    func savestorypiece(storypiece: NSObject) -> (){
+        var newobject = PFObject(className: "StoryPiece")
+        newobject = storypiece as! PFObject
+        newobject.saveInBackgroundWithBlock {
+            (success: Bool, error: NSError?) -> Void in
+            if (success) {
+                print("Deu certo o salve :)")
+            } else {
+               print("Deu errado o salve : /(error)")
+            }
+        }
+        
+    }
 }
+
