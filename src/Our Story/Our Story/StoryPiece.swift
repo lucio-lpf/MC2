29663b04f9c024089f7f114d65301e226823d581
Wagner Santos <prog.wagner@gmail.com>
2015-06-24 13:14:42 -0300

    Initial models structure

---
+++ src/Our Story/Our Story/StoryPiece.swift
@@ -0,0 +1,16 @@
+//
+//  StoryPiece.swift
+//  Our Story
+//
+//  Created by Wagner Santos on 6/24/15.
+//  Copyright (c) 2015 mc2. All rights reserved.
+//
+
+import Parse
+
+class StoryPiece: NSObject {
+    
+    var text: String?
+    var createdBy: User?
+    var createdAt: NSDate?
+}
