21621282d1289c75e5bce9c7a3b1a653085fb83b
Christian S <cschmitz7@icloud.com>
2015-06-29 16:24:57 -0300

    começando a andar com a collection das configs

--- src/Our Story/Our Story/ConfigurationViewController.swift
+++ src/Our Story/Our Story/ConfigurationViewController.swift
@@ -8,12 +8,34 @@
 
 import Foundation
 import UIKit
+import Parse
 
 class ConfigurationViewController: UIViewController {
     
+    @IBOutlet weak var userNameLabel: UILabel!
+    
+    @IBOutlet weak var storyStyleButton: UIButton!
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    var styles = ["cor1","cor2"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.collectionView.alpha = 0
 
+        var user = PFUser.currentUser()
+        userNameLabel.text = user?.objectForKey("name") as? String
+        
+        storyStyleButton.layer.cornerRadius = 10;
+        storyStyleButton.layer.borderColor = UIColor.whiteColor().CGColor
+        storyStyleButton.layer.borderWidth = 2.0
+        //        avatarImage.layer.shadowColor = UIColor.grayColor().CGColor;
+        //        avatarImage.layer.shadowOffset=CGSizeMake(2, 2);
+        //        avatarImage.layer.shadowOpacity=1.0;
+        //        avatarImage.layer.shadowRadius=1.0;
+        storyStyleButton.clipsToBounds = true
         
     }
     
@@ -23,5 +45,22 @@ class ConfigurationViewController: UIViewController {
 
     }
     
+    @IBAction func changeStyleButtonAction(sender: AnyObject) {
+        
+        collectionView.hidden = false
+        animateCollectionViewAlphaFadeIn()
+    }
+    
+    func animateCollectionViewAlphaFadeIn(){
+        UIView.animateWithDuration(1, animations: {
+            self.collectionView.alpha = 1
+        })
+    }
+    
+    func animateCollectionViewAlphaFadeOut(){
+        UIView.animateWithDuration(1, animations: {
+            self.collectionView.alpha = 0
+        })
+    }
     
 }
---
+++ src/Our Story/Our Story/StyleSampleCell.swift
@@ -0,0 +1,17 @@
+//
+//  styleSampleCell.swift
+//  Our Story
+//
+//  Created by Christian S. on 29/06/15.
+//  Copyright (c) 2015 mc2. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class StyleSampleCell: UICollectionViewCell{
+    
+    @IBOutlet weak var imageView: UIImageView!
+
+    var image: UIImage = UIImage()
+}
